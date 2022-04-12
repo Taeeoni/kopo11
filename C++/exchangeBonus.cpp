ce8c5e135c70cf612347b4b8cfa1f8be1c570f92
Taeeoni <102118666+Taeeoni@users.noreply.github.com>
2022-04-12 10:43:34 +0900

    Add files via upload

--- C++/exchangeBonus.cpp
+++ C++/exchangeBonus.cpp
@@ -23,34 +23,32 @@ int main() {
 		float outputDollar = inputWon / DOLLAR_PER_WON;
 		val = (int) ((outputDollar - (int)outputDollar) * DOLLAR_PER_WON);
 		printf("%d 원 -> %d 달러, 잔돈 %d원\n", inputWon, (int)outputDollar, val);
-		nOf1000 = val / 1000;
-		nOf500 = (val % 1000) / 500;
-		nOf100 = (val % 500) / 100;
-		nOf50 = (val % 100) / 50;
-		nOf10 = (val % 50) / 10;
-		nOf1 = (val % 10) / 1;	
-		printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
-		nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );
 	}	
 	//엔
 	if(menuNumber == 2){ 
 		float outputJPY = inputWon / JPY;
 		val = (int) ((outputJPY - (int)outputJPY) * JPY);
 		printf("%d 원 -> %d 엔, 잔돈 %d원\n", inputWon, (int)outputJPY, val);
-		nOf1000 = val / 1000;
-		nOf500 = (val % 1000) / 500;
-		nOf100 = (val % 500) / 100;
-		nOf50 = (val % 100) / 50;
-		nOf10 = (val % 50) / 10;
-		nOf1 = (val % 10) / 1;
-		printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
-		nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );	
 	}
 	//유로
 	if(menuNumber == 3){
 	float outputEUR = inputWon / EUR;
 	val = (int) ((outputEUR - (int)outputEUR) * EUR);
 	printf("%d 원 -> %d 유로, 잔돈 %d원\n", inputWon, (int)outputEUR, val);
+	}
+	//위안
+	if(menuNumber == 4){
+		float outputCNY = inputWon / CNY;
+		val = (int) ((outputCNY - (int)outputCNY) * CNY);
+		printf("%d 원 -> %d 위안, 잔돈 %d원\n", inputWon, (int)outputCNY, val);
+	}
+	//파운드
+	if(menuNumber == 5){
+		float outputGBP = inputWon / GBP;
+		val = (int) ((outputGBP - (int)outputGBP) * GBP);
+		printf("%d 원 -> %d 파운드, 잔돈 %d원\n", inputWon, (int)outputGBP, val);
+
+	}
 	nOf1000 = val / 1000;
 	nOf500 = (val % 1000) / 500;
 	nOf100 = (val % 500) / 100;
@@ -59,34 +57,5 @@ int main() {
 	nOf1 = (val % 10) / 1;	
 	printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
 	nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );
-	}
-	//위안
-	if(menuNumber == 4){
-		float outputCNY = inputWon / CNY;
-		val = (int) ((outputCNY - (int)outputCNY) * CNY);
-		printf("%d 원 -> %d 위안, 잔돈 %d원\n", inputWon, (int)outputCNY, val);
-		nOf1000 = val / 1000;
-		nOf500 = (val % 1000) / 500;
-		nOf100 = (val % 500) / 100;
-		nOf50 = (val % 100) / 50;
-		nOf10 = (val % 50) / 10;
-		nOf1 = (val % 10) / 1;
-		printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
-		nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );	
-	}
-	//파운드
-	if(menuNumber == 5){
-		float outputGBP = inputWon / GBP;
-		val = (int) ((outputGBP - (int)outputGBP) * GBP);
-		printf("%d 원 -> %d 파운드, 잔돈 %d원\n", inputWon, (int)outputGBP, val);
-		nOf1000 = val / 1000;
-		nOf500 = (val % 1000) / 500;
-		nOf100 = (val % 500) / 100;
-		nOf50 = (val % 100) / 50;
-		nOf10 = (val % 50) / 10;
-		nOf1 = (val % 10) / 1;
-		printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
-		nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );	
-	}
 	return 0;
 } 
