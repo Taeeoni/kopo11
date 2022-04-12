eb2bb7ef67028db6d58aa224910ecb1c22b7b59c
Taeeoni <102118666+Taeeoni@users.noreply.github.com>
2022-04-12 10:53:42 +0900

    Add files via upload

---
+++ C++/exchangeBonus2.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+	const float DOLLAR_PER_WON = 1232.90; // 상수는 전부 대문자로 작성하는게 약속 
+	const float JPY = 9.87;
+	const float EUR = 1342.53;
+	const float CNY = 193.20;
+	const float GBP = 1605.22;
+		
+	int inputWon;
+	printf("환전을 원하는 원화 금액을 입력하세요 : ");
+	scanf("%d", &inputWon);	
+	
+	int menuNumber;
+	do{
+		printf("환전할 외화를 선택하세요 (1:USD, 2:JPY, 3:EUR, 4:CNY, 5:GBP) : ");
+		scanf("%d", &menuNumber);
+	} while(menuNumber < 1 || menuNumber > 5);
+	
+	int val; int nOf1000; int nOf500; int nOf100; int nOf50; int nOf10; int nOf1;
+	float outputMoney; float selectCountry;
+	//달러 
+	if(menuNumber == 1){
+		selectCountry = DOLLAR_PER_WON;
+	}	
+	//엔
+	if(menuNumber == 2){ 
+		selectCountry = JPY;
+	}
+	//유로
+	if(menuNumber == 3){
+		selectCountry = EUR;
+	}
+	//위안
+	if(menuNumber == 4){
+		selectCountry = CNY;
+	}
+	//파운드
+	if(menuNumber == 5){
+		selectCountry = GBP;
+	}
+	
+	outputMoney = inputWon / selectCountry;
+	val = (int) ((outputMoney - (int)outputMoney) * selectCountry);
+	printf("%d 원 -> %d 달러, 잔돈 %d원\n", inputWon, (int)outputMoney, val);
+	
+	nOf1000 = val / 1000;
+	nOf500 = (val % 1000) / 500;
+	nOf100 = (val % 500) / 100;
+	nOf50 = (val % 100) / 50;
+	nOf10 = (val % 50) / 10;
+	nOf1 = (val % 10) / 1;	
+	printf("1000원 : %d장\n500원 : %d개\n100원 : %d개\n50원 : %d개\n10원 : %d개\n1원 : %d개\n" , 
+	nOf1000, nOf500, nOf100, nOf50, nOf10, nOf1 );
+	return 0;
+} 
